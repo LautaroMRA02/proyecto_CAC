9502cb17ce743500cc1e2fe2145bc74150f195a7
Lautaro Rivadero <lautaro.mra.2002@gmail.com>
2021-10-20 19:41:42 -0300

    ..

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 function validarCorreo(correo){
-    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  //  var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var expReg= /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]/g;
     var esValido= expReg.test(correo);
     if(esValido==true){
         alert('Correo valido.');
